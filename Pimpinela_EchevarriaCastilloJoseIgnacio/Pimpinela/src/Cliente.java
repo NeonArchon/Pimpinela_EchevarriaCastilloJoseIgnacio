be14e0bd4a8d83e51487f8345ac0f797275a5b6f
Jose <mortox1990@gmail.com>
2025-01-28 20:25:31 +0100

    Terminando la funcionalidad de las clases

--- Pimpinela_EchevarriaCastilloJoseIgnacio/Pimpinela/src/Cliente.java
+++ Pimpinela_EchevarriaCastilloJoseIgnacio/Pimpinela/src/Cliente.java
@@ -6,24 +6,39 @@ import java.net.Socket;
 public class Cliente {
     public static void main(String[] args) {
 
-        String HOST ="127.0.0.1";
-        int PUERTO = 5000;
-        DataInputStream in;
-        DataOutputStream out;
+        String HOST ="127.0.0.1"; //definor dereccion IP para conectarr
+        int PUERTO = 5000; //puerto del servidor
+        //variables para manejo de datos
+        DataInputStream in; //leer datos del cocket
+        DataOutputStream out; //enviar datos del socker
 
         try{
-
+            //creacion de ub socket
             Socket sc = new Socket(HOST,PUERTO);
 
+            //inicielizacion de flujos de entrada y salida del socker
             in = new DataInputStream(sc.getInputStream());
             out = new DataOutputStream(sc.getOutputStream());
 
-            out.writeUTF("¿Quien es?");
+            //array con todas las respuestas posibles
+            String[] mensajes = {"¿Quién es?",
+                                 "¿Qué vienes a buscar?",
+                                 "Ya es tarde",
+                                 "Porque ahora soy yo la que quiere estar sin ti"};
 
-            String msg = in.readUTF();
+            //bucle para el envio de las respuestas
+            for (String mensaje : mensajes) {
+                out.writeUTF(mensaje);
+                String respuesta = in.readUTF();
+                System.out.println("Servidor: " + respuesta);
 
-            System.out.println(msg);
+                if ("Error".equals(respuesta)) {
+                    System.out.println("Ocurrio un error de en la conexion");
+                    break;
+                }
+            }
 
+            //cerrar socket
             sc. close();
 
         } catch (IOException e) {
--- Pimpinela_EchevarriaCastilloJoseIgnacio/Pimpinela/src/Main.java
+++ Pimpinela_EchevarriaCastilloJoseIgnacio/Pimpinela/src/Main.java
@@ -7,31 +7,81 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) {
 
-        ServerSocket server = null;
-        Socket sc;
+        ServerSocket server = null; //para escuchar las conexiones con el cliente
+        Socket sc; //conexion del cliene
         DataInputStream in;
         DataOutputStream out;
         int PUERTO = 5000;
 
         try{
+            //inicializacion del socket
             server = new ServerSocket(PUERTO);
             System.out.println("servidor iniciado");
 
             //canal siempre abierto
             while (true) {
                 //socket del cliente
-
                 sc = server.accept(); //se queda escuchando, esperoando respuesta
-
                 System.out.println("Conectando con Cliente");
 
+                //inicializacion de los flujos de entrada y salida de datos
                 in = new DataInputStream(sc.getInputStream()); //recibe mensajes
-                out = new DataOutputStream(sc.getOutputStream());//envia mensajes
+                out = new DataOutputStream(sc.getOutputStream());//envia mensajesç
 
-                out.writeUTF("Soy yo");
+                String msg = in.readUTF();
+                System.out.println(msg);
 
+                String estado = "inicio";
+                boolean Conversacion = true;
+
+                //bucle para responder al cliente
+                while (Conversacion) {
+                    String MensajeCliente = in.readUTF();
+                    System.out.println("Cliente" + MensajeCliente);
+
+                    switch(estado) {
+                        case "inicio":
+                            if ("¿Quien es?".equals(MensajeCliente)){
+                                //enviar mensaje al cliente
+                                out.writeUTF("Soy Yo");
+                                estado = "Pregunta1";
+                            } else {
+                                out.writeUTF("ERROR");
+                            }
+                            break;
+
+                        case "pregunta1":
+                            if ("¿Qué vienes a buscar?".equals(MensajeCliente)) {
+                                out.writeUTF("A ti");
+                                estado = "pregunta2";
+                            } else {
+                                out.writeUTF("Error");
+                            }
+                            break;
+
+                        case "pregunta2":
+                            if ("Ya es tarde".equals(MensajeCliente)) {
+                                out.writeUTF("¿Por qué?");
+                                estado = "pregunta3";
+                            } else {
+                                out.writeUTF("Error");
+                            }
+                            break;
+
+                        case "pregunta3":
+                            if ("Porque ahora soy yo la que quiere estar sin ti".equals(MensajeCliente)) {
+                                out.writeUTF("Por eso vete, olvida mi nombre, mi cara, mi casa y pega la vuelta");
+                                Conversacion = false;
+                            } else {
+                                out.writeUTF("Error");
+                            }
+                            break;
+
+                    }
+                }
+
+                //cerrar el socket
                 sc.close();
-
                 out.writeUTF("Cliente desconectado");
 
             }
